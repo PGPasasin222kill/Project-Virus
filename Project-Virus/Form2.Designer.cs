7950b851e75dc3e0c46d86266c17255a7f5f4817
Elev <Elev@INFO-27>
2025-12-02 09:43:28 +0200

    0.2.6 harta+tari fix

--- Project-Virus/Form2.Designer.cs
+++ Project-Virus/Form2.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(569, 12);
+            this.button1.Location = new System.Drawing.Point(800, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 34);
             this.button1.TabIndex = 0;
@@ -44,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(569, 62);
+            this.button2.Location = new System.Drawing.Point(801, 52);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(96, 33);
             this.button2.TabIndex = 1;
@@ -52,17 +55,28 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(753, 820);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 527);
+            this.ClientSize = new System.Drawing.Size(941, 824);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -71,5 +85,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Project-Virus/Form2.cs
+++ Project-Virus/Form2.cs
@@ -52,12 +52,17 @@ namespace Project_Virus
             this.MouseDown += Form2_MouseDown;
             this.MouseMove += Form2_MouseMove;
             this.MouseUp += Form2_MouseUp;
+
+            pictureBox1.Paint += PictureBox1_Paint;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             // Noduri
-            nodes.Add(new Node(400, 300, "Romania"));//0
+            nodes.Add(new Node(1000, 300, "Romania"));//0
             nodes.Add(new Node(300, 50, "Bulgaria"));//1
             nodes.Add(new Node(200, 150, "Serbia"));//2
             nodes.Add(new Node(250, 400, "Ungaria"));//3
@@ -67,6 +72,14 @@ namespace Project_Virus
             nodes.Add(new Node(450, 400, "Slovacia"));//7
             nodes.Add(new Node(200, 450, "Slovenia"));//8
             nodes.Add(new Node(250, 450, "Polonia"));//9
+            nodes.Add(new Node(250, 450, "Germania"));//10
+            nodes.Add(new Node(250, 450, "Cehia"));//11
+            nodes.Add(new Node(250, 450, "Austria"));//12
+            nodes.Add(new Node(250, 450, "Croatia"));//13
+            nodes.Add(new Node(250, 450, "Elvetia"));//14
+            nodes.Add(new Node(250, 450, "Italia"));//15
+            nodes.Add(new Node(250, 450, "Bosnia"));//16
+
 
             // Muchii
             edges.Add(new Edge(nodes[1], nodes[0]));
@@ -78,15 +91,38 @@ namespace Project_Virus
             edges.Add(new Edge(nodes[7], nodes[3]));
             edges.Add(new Edge(nodes[8], nodes[3]));
             edges.Add(new Edge(nodes[9], nodes[7]));
+            edges.Add(new Edge(nodes[9], nodes[4]));
+            edges.Add(new Edge(nodes[5], nodes[4]));
+            edges.Add(new Edge(nodes[7], nodes[4]));
+            edges.Add(new Edge(nodes[2], nodes[1]));
+            edges.Add(new Edge(nodes[3], nodes[2]));
+            edges.Add(new Edge(nodes[10], nodes[9]));
+            edges.Add(new Edge(nodes[10], nodes[11]));
+            edges.Add(new Edge(nodes[11], nodes[12]));
+            edges.Add(new Edge(nodes[11], nodes[7]));
+            edges.Add(new Edge(nodes[8], nodes[13]));
+            edges.Add(new Edge(nodes[2], nodes[13]));
+            edges.Add(new Edge(nodes[10], nodes[12]));
+            edges.Add(new Edge(nodes[12], nodes[3]));
+            edges.Add(new Edge(nodes[12], nodes[7]));
+            edges.Add(new Edge(nodes[12], nodes[8]));
+            edges.Add(new Edge(nodes[14], nodes[15]));
+            edges.Add(new Edge(nodes[14], nodes[10]));
+            edges.Add(new Edge(nodes[14], nodes[12]));
+            edges.Add(new Edge(nodes[14], nodes[15]));
+            edges.Add(new Edge(nodes[16], nodes[2]));
+            edges.Add(new Edge(nodes[16], nodes[13]));
+            edges.Add(new Edge(nodes[3], nodes[13]));
+            edges.Add(new Edge(nodes[8], nodes[15]));
             LoadGraph();
         }
 
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen edgePen = new Pen(Color.Black, 2);
-            Brush nodeBrush = Brushes.LightBlue;
-            Font font = new Font("Arial", 12);
+            Pen edgePen = new Pen(Color.Black, 3);
+            Brush nodeBrush = Brushes.LightGreen;
+            Font font = new Font("Bold", 10);
 
             // Desenează muchiile
             foreach (Edge edge in edges)
@@ -152,22 +188,75 @@ namespace Project_Virus
         {
             selectedNode = null;
         }
-        private void SaveGraph()
-        {
-            using (StreamWriter sw = new StreamWriter("graf.txt"))
-            {
-                foreach (Node node in nodes)
-                {
-                    sw.WriteLine($"{node.Name}:{node.X},{node.Y}");
-                }
-            }
-        }
 
         private void button2_Click_1(object sender, EventArgs e)
         {
             SaveGraph();
         }
 
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            Pen edgePen = new Pen(Color.Black, 1);
+            Brush nodeBrush = Brushes.LightGreen;
+            Font font = new Font("Arial", 10, FontStyle.Bold);
+
+            // Desenează muchiile
+            foreach (Edge edge in edges)
+            {
+                g.DrawLine(edgePen,
+                           edge.From.X, edge.From.Y,
+                           edge.To.X, edge.To.Y);
+            }
+
+            // Desenează nodurile
+            int radius = 20;
+            foreach (Node node in nodes)
+            {
+                g.FillEllipse(nodeBrush, node.X - radius, node.Y - radius, radius * 2, radius * 2);
+                g.DrawEllipse(Pens.Black, node.X - radius, node.Y - radius, radius * 2, radius * 2);
+
+                SizeF textSize = g.MeasureString(node.Name, font);
+                float textX = node.X - textSize.Width / 2;
+                float textY = node.Y - radius - textSize.Height - 2;
+
+                g.DrawString(node.Name, font, Brushes.Black, textX, textY);
+            }
+        }
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int radius = 20;
+
+            foreach (var node in nodes)
+            {
+                int dx = e.X - node.X;
+                int dy = e.Y - node.Y;
+
+                if (dx * dx + dy * dy <= radius * radius)
+                {
+                    selectedNode = node;
+                    offsetX = dx;
+                    offsetY = dy;
+                    break;
+                }
+            }
+        }
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedNode != null && e.Button == MouseButtons.Left)
+            {
+                selectedNode.X = e.X - offsetX;
+                selectedNode.Y = e.Y - offsetY;
+
+                pictureBox1.Invalidate(); // redesenează graful
+            }
+        }
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            selectedNode = null;
+        }
         private void LoadGraph()
         {
             if (!File.Exists("graf.txt"))
@@ -194,6 +283,16 @@ namespace Project_Virus
             }
 
             this.Invalidate(); // redesenează nodurile
+        }
+        private void SaveGraph()
+        {
+            using (StreamWriter sw = new StreamWriter("graf.txt"))
+            {
+                foreach (Node node in nodes)
+                {
+                    sw.WriteLine($"{node.Name}:{node.X},{node.Y}");
+                }
+            }
         }
 
     }
